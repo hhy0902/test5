338cc6d3ebd4daa6426f631926c92905df5d1ef4
hhy0902 <hhy09020@naver.com>
2024-07-25 15:15:25 +0900

    0725 -2

--- lib/main.dart
+++ lib/main.dart
@@ -20,3 +20,44 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
